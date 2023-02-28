883c5a79dd9d10ae9fab43a6f55d965a1365027c
Fricoue <62333955+Fricoue@users.noreply.github.com>
2023-02-28 09:39:25 +0100

    Renommage fichiers

---
+++ src/main/java/com/example/memoire/MemoryController.java
@@ -0,0 +1,10 @@
+package com.example.memoire;
+
+import javafx.fxml.FXML;
+
+public class MemoryController {
+    @FXML
+    protected void nouvellePartie() {
+        System.out.println("new game");
+    }
+}
